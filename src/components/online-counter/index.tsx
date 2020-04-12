72a115576465ce89e5152b8e5ba2d326b01ae6ea
Lucas Reis <thereis@live.com>
2020-04-11 23:34:05 -0300

    fix: updated online counter

--- src/components/online-counter/index.tsx
+++ src/components/online-counter/index.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
  * Dependencies
  */
 import { HomeService } from '../../modules/home/home.service';
+import { animateCSS } from '../../modules/home/utils';
 
 /**
  * Components
@@ -16,13 +17,15 @@ import { Users } from 'react-feather';
 import './index.style.scss';
 
 interface IProps {
-  mode?: 'index' | 'client';
+  mode?: 'index' | 'client' | 'home';
 }
 
-const OnlineCounter: React.FC<IProps> = props => {
+const OnlineCounter: React.FC<IProps> = (props) => {
   const [onlines, setOnlines] = React.useState<number>();
   const [retry, setRetry] = React.useState<number>(0);
   const [error, setError] = React.useState<Error>();
+
+  const counterHomeEl = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     let id = setInterval(() => {
@@ -38,6 +41,10 @@ const OnlineCounter: React.FC<IProps> = props => {
       const onlines = await HomeService.getUsersOnlineCount();
 
       setOnlines(onlines.usersOnline);
+
+      if (counterHomeEl.current) {
+        animateCSS(counterHomeEl.current, 'flash');
+      }
     };
 
     try {
@@ -47,9 +54,28 @@ const OnlineCounter: React.FC<IProps> = props => {
     }
   }, [retry]);
 
-  return (
-    <div className="">
-      {props.mode === 'client' ? (
+  switch (props.mode) {
+    case 'home':
+      return (
+        <div className="flex flex-col w-32 h-24 bg-white rounded justify-center border border-dGray select-none">
+          <div className="flex justify-center items-center p-1">
+            <img
+              className="p-2"
+              src="/assets/images/icons/headCounter.png"
+              alt="habbos"
+            />
+            <p ref={counterHomeEl} className="font-semibold mt-2">
+              {!onlines ? `...` : `${onlines}`}
+            </p>
+          </div>
+          <div className="text-center">
+            <p className="text-sm">Habbos conectados!</p>
+          </div>
+        </div>
+      );
+
+    case 'client':
+      return (
         <div className="p-1 flex items-center justify-center animated pulse slower infinite border-gray-500 bg-gray-900 border-solid border-2 cursor-default select-none rounded">
           <Users className="text-white" size={16} />
 
@@ -63,7 +89,10 @@ const OnlineCounter: React.FC<IProps> = props => {
             )}
           </div>
         </div>
-      ) : (
+      );
+
+    default:
+      return (
         <div className="px-2 py-2 flex rounded font-thin bg-white rounded-full p-3 text-center">
           <p className="flex text-gray-700">
             {error && <>{error.message}</>}
@@ -75,9 +104,8 @@ const OnlineCounter: React.FC<IProps> = props => {
                 }!`}
           </p>
         </div>
-      )}
-    </div>
-  );
+      );
+  }
 };
 
 export default OnlineCounter;
