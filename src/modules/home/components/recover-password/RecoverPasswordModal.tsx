6b4867c58943432b8c1e0d08e788ff5af21cd701
Luciano Liboni <luciano_liboni@hotmail.com>
2020-05-15 00:15:01 -0300

    add captcha in recover and change password (#12)

--- src/modules/home/components/recover-password/RecoverPasswordModal.tsx
+++ src/modules/home/components/recover-password/RecoverPasswordModal.tsx
@@ -6,6 +6,8 @@ import * as React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { HomeService } from '../../../home/home.service';
+import { useMount } from 'react-use';
+import { useApp } from '../../../../App.context';
 
 /**
  * Components
@@ -15,37 +17,67 @@ import ModalBody from '../../../../components/modal/modal-body';
 import Input from '../../../../components/input';
 import { Mail } from 'react-feather';
 import { RecoverPasswordDTO } from '../../models/recover-password.dto';
+import RecaptchaContainer from '../../../../components/recaptcha';
+import LoadingSpinner from '../../../../components/spinner';
 import { toast } from 'react-toastify';
 
 const RescoverPasswordModal: React.FC = () => {
   const { register, handleSubmit, errors } = useForm();
-
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
-
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [isMounting, setIsMounting] = React.useState(true);
+  const [recaptchaToken, setRecaptchaToken] = React.useState<string>('');
+  
   const history = useHistory();
+  const [state] = useApp();
   
   const _goBack = () => {
     history.push('/');
   };
+
+  useMount(() => {
+    if (state.accessToken) {
+      history.push('/');
+    }
+    setIsMounting(false);
+  });
 
   const isDisabled = isLoading;
   const disabledCSS = isDisabled ? 'opacity-50 cursor-not-allowed' : '';
 
   const onSubmit = async(data: Record<string, string>) => {
     try {
-      setIsLoading(true);
 
-      const formData = new RecoverPasswordDTO(data);
+      const formData = new RecoverPasswordDTO({...data, recaptchaToken});
 
-      await HomeService.recoverPassword(formData);
-      
+      await HomeService.recoverPassword(formData, recaptchaToken);
+
       toast.success(`O link de recuperação foi enviado para o e-mail ${formData.mail}`)
 
+      _goBack();
+
     } catch(err){
-      toast.error(`Não foi possível enviar o e-mail para ${data.mail}. Verifique e tente novamente!`);
+      toast.error(`Não foi possível enviar o e-mail para ${data.mail}. Verifique o e-mail e tente novamente!`);
       
       _goBack();
+    } finally {
+      setIsLoading(false);
     }
+  }
+
+  const _handleCaptcha = (value: string | null) => {
+    if (recaptchaToken && value === null) return setRecaptchaToken('');
+    if (value === null) return;
+
+    setIsLoading(false);
+    setRecaptchaToken(value);
+  };
+
+  const _handleRecaptchaError = () => {
+    setRecaptchaToken('');
+  };
+
+  if (isMounting) {
+    return <LoadingSpinner />;
   }
 
   return (
@@ -66,8 +98,15 @@ const RescoverPasswordModal: React.FC = () => {
               }}
             />
           </div>
-
-          <div className="flex items-center justify-center">
+          
+            <div className="flex justify-center mt-5">
+              <RecaptchaContainer
+                onErrored={_handleRecaptchaError}
+                onExpired={_handleRecaptchaError}
+                onChange={_handleCaptcha}
+              />
+            </div>
+            <div className="flex items-center justify-center m-4">
             <div className='pl-50%'>
             <button
                   className={`text-center focus:outline-none modal-close px-4 p-3 text-center rounded-lg text-white bg-blue-600 hover:bg-blue-800 ${disabledCSS}` }
--- src/modules/home/models/change-password.dto.ts
+++ src/modules/home/models/change-password.dto.ts
@@ -1,8 +1,8 @@
 export class ChangePasswordDTO {
   password!: string;
   passwordConfirmation!: string;
-
-
+  recaptchaToken!: string;
+  
   constructor(params: Partial<ChangePasswordDTO>){
     Object.assign(this, params);
   }
